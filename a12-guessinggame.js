bbd2061ead60b4090f022e65e2f8e4911946448d
frknslck <furkanselcuk.fs58@gmail.com>
2022-12-20 22:20:19 +0300

    new assingments added

---
+++ a12-guessinggame.js
@@ -0,0 +1,18 @@
+// const randomnum = Math.floor(Math.random() * 100)
+// let life = 5;
+// let guess = ""
+// do {
+//     guess = +prompt("Enter your guess: ");
+//     if (guess == randomnum){
+//         console.log("Congrats! You guessed right!");
+//         break;
+//     }else {
+//         life -= 1;
+//         if (guess < randomnum){
+//             console.log("Wrong! Try higher.");
+//         }else {
+//             console.log("Wrong! Try Lower.");
+//         }
+//     }
+// } while (life != 0);
+// console.log("What a shame... Maybe next time.");
